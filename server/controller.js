aef6306cf67dddaa9e5c38397d7063521317faac
Kristian Workinger <kristianworkinger@gmail.com>
2021-11-02 16:29:58 -0600

    real estate back end lab 2

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,44 @@
 const { request, response} = require("express")
-const 
+const houses = require("./db.json")
+globalId = 4
 
-module.exports()
+module.exports = {
+    getHouses: (request, response) => {
+        response.status(200).send(houses)
+    },
+    deleteHouse: (request, response) => {
+        let index = houses.findIndex(elem => elem.id === +request.params.id)
+        houses.splice(index, 1)
+        response.status(200).send(houses)
+    },
+    createHouse: (request, response) => {
+        //give the request an id and store the data provided in the back end
+        let {address, price, imageURL} = request.body
+        let newHouse = {
+            id: globalId,
+            address,
+            price: +price,
+            imageURL: imageURL
+        }
+        houses.push(newHouse)
+        response.status(200).send(houses)
+        globalId++
+    },
+    updateHouse: (request, response) => {
+        let {id} = request.params
+        let {type} = request.body
+        let index = houses.findIndex(elem => elem.id === +id)
+            console.log("hello world")        
+        if (houses[index].price >= 10000000) {
+          response.status(400).send("We only sell homes below 10 million US dollars")  
+        } else if (houses[index].price <= 100000) {
+            response.status(400).send("We do not sell homes below 100K US dollars")
+        } else if (type === "minus") {
+            houses[index].price -= 10000
+            response.status(200).send(houses)
+        } else if (type === "plus") {
+            houses[index].price += 10000
+            response.status(200).send(houses)
+        }
+    }
+}
--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const {getHouses, deleteHouse, createHouse, updateHouse} = require("./controller.js")
+
+app.get("/api/houses", getHouses)
+app.delete("/api/houses/:id", deleteHouse)
+app.post("/api/houses", createHouse)
+app.put("/api/houses/:id", updateHouse)
 
 
 app.listen(4004, () => {
